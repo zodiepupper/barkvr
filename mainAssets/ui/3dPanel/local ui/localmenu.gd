7e402a1d2c218733b3d545bf0f3a58840a7915c6
zodywoolsey <zodie@pupper.dev>
2025-05-22 07:30:00 -0600

    finally setup the menu spawn button on the context menu

--- mainAssets/ui/3dPanel/local ui/localmenu.gd
+++ mainAssets/ui/3dPanel/local ui/localmenu.gd
@@ -22,17 +22,25 @@ func _ready():
 		,4)
 	if get_viewport().get_parent() is Panel3D:
 		get_viewport().get_parent().minimum_viewport_size = Vector2i(small_height,small_height)
-	button.pressed.connect(func():
-		expanded = !expanded
-		resize.visible = expanded
-		tab_container.visible = expanded
-		if get_viewport().get_parent() is Panel3D:
-			create_tween().tween_property(get_viewport().get_parent(),"viewport_size:y",big_height if expanded else small_height,.4).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-			create_tween().tween_property(get_viewport().get_parent(),"viewport_size:x",big_width if expanded else small_height,1.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
-		)
+	button.pressed.connect(reveal)
 
 func _input(event:InputEvent):
 	if resize.button_pressed and event is InputEventMouseMotion and get_viewport().get_parent() is Panel3D:
 		big_height = event.position.y
 		big_width = event.position.x if event.position.x > tab_container.custom_minimum_size.x else tab_container.custom_minimum_size.x
 		get_viewport().get_parent().viewport_size = Vector2i(big_width, big_height)
+
+func reveal(force_open:bool=false) -> void:
+	if force_open:
+		expanded = true
+		get_viewport().get_parent().viewport_size.x = big_height
+		get_viewport().get_parent().viewport_size.y = big_width
+		resize.visible = expanded
+		tab_container.visible = expanded
+		return
+	expanded = !expanded
+	resize.visible = expanded
+	tab_container.visible = expanded
+	if get_viewport().get_parent() is Panel3D:
+		get_viewport().get_parent().viewport_size.x = big_height if expanded else small_height
+		get_viewport().get_parent().viewport_size.y = big_width if expanded else small_height

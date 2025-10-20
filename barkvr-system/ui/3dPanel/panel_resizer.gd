452d7295f2abbadeb3030191308905679aa61e0a
zodywoolsey <zodie@pupper.dev>
2025-10-19 18:45:47 -0600

    added 3dPanel resizer helper thing, updated unified inspector, created single node color from hash background color rect thing lol

---
+++ barkvr-system/ui/3dPanel/panel_resizer.gd
@@ -0,0 +1,12 @@
+class_name PanelResizer
+extends Button
+
+func _ready() -> void:
+	focus_mode = Control.FOCUS_NONE
+
+func _input(event: InputEvent) -> void:
+	if button_pressed and event is InputEventMouseMotion and get_viewport().get_parent() is Panel3D:
+		get_viewport().size.y = event.position.y as int
+		get_viewport().size.x = event.position.x as int
+		if get_viewport().get_parent() is Panel3D:
+			get_viewport().get_parent().viewport_size = Vector2i(get_viewport().size.x, get_viewport().size.y)

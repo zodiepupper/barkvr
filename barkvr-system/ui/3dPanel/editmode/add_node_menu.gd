35445aa9c9f64eec306d3316123066360add42c6
Holy-Person <holyochi@gmail.com>
2025-12-01 01:18:39 +0100

    AddNodeMenu Basic Functionality
    Major cleanup of the pre-rework script, comments have been added to most pieces of code to elaborate on what is being done.
    Certain parts of code were re-written to improve efficiency and readability.
    
    Compared to the previous version this menu requires confirmation to add a node, instead of applying it on selection.
    Double clicking is broken due to (assumed) 3DUI limitations, and will be custom-made in another commit.

---
+++ barkvr-system/ui/3dPanel/editmode/add_node_menu.gd
@@ -0,0 +1,135 @@
+extends Control
+
+## A theme containing all of the godot default icons, used to display node types in the item list.
+const GODOT_EDITOR_ICON_THEME = preload("uid://b34aw2colacks")
+
+var target : Node
+var event_manager : Bark_Journal
+
+@onready var window_icon: TextureRect = %WindowIcon
+
+@onready var search_bar: LineEdit = %SearchBar
+@onready var item_list: ItemList = %ItemList
+
+@onready var button_close: Button = %ButtonClose
+@onready var button_confirm: Button = %ButtonConfirm
+@onready var button_cancel: Button = %ButtonCancel
+
+
+
+func _ready() -> void:
+	event_manager = Engine.get_singleton(&"event_manager")
+	print("event supplier: " + str(event_manager))
+
+	_setup_icons()
+	_load_class_list()
+
+	button_close.pressed.connect(close)
+	button_cancel.pressed.connect(close)
+	button_confirm.pressed.connect(add_selected_node)
+
+	search_bar.text_changed.connect(_on_search_bar_edited)
+	item_list.item_selected.connect(_on_item_list_item_selected)
+	# item_activated just does not work with 3DUI apparently, real fun.
+	# Currently using a custom double click checker using item_selected.
+	#item_list.item_activated.connect(_on_item_list_item_activated)
+
+## Set up dynamic icons loaded from the editor theme or project settings.
+func _setup_icons() -> void:
+	window_icon.set_texture(load(ProjectSettings.get_setting("application/config/icon")))
+	search_bar.set_right_icon(GODOT_EDITOR_ICON_THEME.get_icon(&"Search", &"EditorIcons"))
+	button_close.set_button_icon(GODOT_EDITOR_ICON_THEME.get_icon(&"Close", &"EditorIcons"))
+
+## Load the initial, unfiltered, class list.
+func _load_class_list() -> void:
+	for cls : String in ClassDB.get_class_list() + Bark_Journal.extra_classes:
+		if not ClassDB.is_parent_class(cls, &"Node") and not cls in Bark_Journal.extra_classes: continue
+
+		add_class_to_item_list(cls)
+
+	item_list.select(0)
+
+
+
+func set_target(item : Node) -> void:
+	target = item
+
+## Confirm selection and add it to the target as a child.
+func add_selected_node() -> void:
+	if !is_instance_valid(target): return
+	if !is_instance_valid(event_manager): return
+
+	var selected_index : int = item_list.get_selected_items()[0]
+	var selected_class : String = item_list.get_item_text(selected_index)
+
+	event_manager.add_node(event_manager.root.get_path_to(target),{
+		"node_class" : selected_class,
+		"properties" : [
+			{
+				"name" : "metadata/display_name",
+				"value" : selected_class
+			}
+		]
+	})
+	close()
+
+## Hide the menu and reset selection to the first.
+func close() -> void:
+	item_list.deselect_all()
+	item_list.select(0)
+	hide()
+
+## Add an item to the item list with class_string as the type.
+func add_class_to_item_list(class_string : String) -> void:
+	if GODOT_EDITOR_ICON_THEME.has_icon(class_string, &"EditorIcons"):
+		item_list.add_item(class_string, GODOT_EDITOR_ICON_THEME.get_icon(class_string, &"EditorIcons"))
+	else: # Fallback.
+		item_list.add_item(class_string, GODOT_EDITOR_ICON_THEME.get_icon(&"Node", &"EditorIcons"))
+
+## Search through the item list.
+func _on_search_bar_edited(search_text : String) -> void:
+	search_text = search_text.to_lower()
+
+	# Deselect items and clear list.
+	item_list.deselect_all()
+	item_list.clear()
+
+	var filtered_list := Array()
+	var class_list : PackedStringArray = ClassDB.get_class_list()
+	class_list.append_array(Bark_Journal.extra_classes)
+
+	for class_string : String in class_list:
+		if ( # Discard unfit classes.
+				not ClassDB.is_parent_class(class_string, &"Node")
+				and not class_string in Bark_Journal.extra_classes
+		): continue
+
+		var contains_all_chars := true
+		var class_string_lower := class_string.to_lower()
+		for character in search_text:
+			if !class_string_lower.contains(character):
+				contains_all_chars = false
+				break
+		if (
+				contains_all_chars
+				or class_string_lower.contains(search_text)
+				or class_string_lower.similarity(search_text) > .6
+		):
+			filtered_list.append(class_string)
+
+	# Sort the list to make the "most accurate" result the top item.
+	filtered_list.sort_custom(func(a : String, b : String) -> bool:
+		return true if search_text.similarity(a.to_lower()) > search_text.similarity(b.to_lower()) else false
+	)
+	# Populate list with search matches.
+	for item : String in filtered_list:
+		add_class_to_item_list(item)
+
+	# Select the first item to prevent no items being selected.
+	item_list.select(0)
+
+## Called when an item is selected, used to detect double clicks.
+func _on_item_list_item_selected(_index :int) -> void:
+	# TODO: This right here.
+	#print("item selected")
+	pass

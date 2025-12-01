293a1a75b9699f9456a2c7fb90afe9c542b45d20
Holy-Person <holyochi@gmail.com>
2025-12-02 00:37:21 +0100

    Set Targets In Both Tabs
    More improvements to be made here.
    I plan to make the panels able to be taken out and rearranged without issues.

--- barkvr-system/ui/3dPanel/editmode/unified editor/inspector_rework.gd
+++ barkvr-system/ui/3dPanel/editmode/unified editor/inspector_rework.gd
@@ -1,21 +1,29 @@
 extends Control
 
+
+
+@onready var tab_left: TabContainer = %TabLeft
+@onready var tab_right: TabContainer = %TabRight
+
 ## The left side of the inspector, a scene inspector.
 @onready var scene_inspector: Control = %SceneInspector
-## Container holding all the context-sensitive panels on the right side of the inspector.
-@onready var tab_container: TabContainer = %TabContainer
 
 
 
 func _ready() -> void:
-	scene_inspector.selection_changed.connect(_on_node_selection_change)
+	scene_inspector.selection_changed.connect(_on_node_selection_changed)
+
+
 
 ## Called when a new node gets selected from the scene inspector.
-func _on_node_selection_change(new_selection : Node) -> void:
+func _on_node_selection_changed(new_selection : Node) -> void:
 	# Prevent several attribute panels from loading at the same time.
 	LocalGlobals.is_inspector_loading = false
 
-	# Set target on all tabs.
-	for tab : Control in tab_container.get_children():
+	# Get all currently attached tabs.
+	var target_tabs : Array[Node] = tab_left.get_children() + tab_right.get_children()
+
+	# Set target on all possible tabs.
+	for tab : Node in target_tabs:
 		if tab.has_method(&"set_target"):
 			tab.set_target(new_selection)

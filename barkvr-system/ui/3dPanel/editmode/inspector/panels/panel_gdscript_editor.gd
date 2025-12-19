642ee4111dcb987fc04d2729a0c16982a5b52f7f
Holy-Person <holyochi@gmail.com>
2025-12-19 22:20:06 +0100

    Giant Mess, Read Description
    I have been working on & off on this for quite a while now, finally somewhat settling on some way of doing this.
    All of the commits that get pushed with this one are part of this gigantic mess of constant retries on how to actually realize this thing.
    There will be a few more somewhat major changes to the way the UI is done, but the basics are mostly in place with no more issues.

--- barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_gdscript_editor.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/panels/panel_gdscript_editor.gd
@@ -2,24 +2,13 @@ extends InspectorPanel
 
 
 
-## A GDScript template used to fill out the CodeEdit when no script is present.
-const ScriptTemplate = preload("uid://cjfm48dxhjdhi")
-
-## The ScriptTemplate as a String.
-## "extends" is missing due to it being auto-generated upon selection.
-var script_template_text : String
-
 ## A setting on whether the code should be saved on any text change in the CodeEdit.
-var save_on_edit : bool = false # TODO: True by default, false for testing.
+var save_on_edit: bool = false # TODO: True by default, false for testing.
+var sort_method_list_alphabetically: bool = false
 
-var sort_method_list_alphabetically : bool = false
-
-## The name of the currently selected script.
-## Suffixed with "(*)" when the script has unsaved changes.
-var current_script_name : String:
-	set(new_name):
-		current_script_name = new_name
-		label_script_name.text = new_name
+## A menu bar used to display script and documentation related options.
+## Buttons in the bar and their submenus adjust based on current context.
+@onready var top_menu_bar: ScriptMenuBar = %TopMenuBar
 
 ## A label to show the current script name.
 @onready var label_script_name: Label = %LabelScriptName
@@ -34,42 +23,34 @@ var current_script_name : String:
 @onready var sidebar: VSplitContainer = %Sidebar
 @onready var search_scripts: LineEdit = %SearchScripts
 @onready var list_scripts: ItemList = %ListScripts
+@onready var sidebar_methods: VBoxContainer = %SidebarMethods
 @onready var search_methods: LineEdit = %SearchMethods
 @onready var button_sort_methods: Button = %ButtonSortMethods
 @onready var list_methods: ItemList = %ListMethods
 
-## The central CodeEdit.
-@onready var code_edit: CodeEdit = %CodeEdit
+## The central TabContainer holding GDScriptCodeEdits and GDScriptDocumentations.
+@onready var script_tab_container: ScriptTabContainer = %ScriptTabContainer
 
 ## A button to toggle the sidebar, uses an icon that changes based on sidebar state.
 @onready var button_toggle_sidebar: Button = %ButtonToggleSidebar
+## A container for all script-specific elements.
+## These might actually be part of each ScriptTabContainer child in the original.
+## However this results in the loss of the sidebar toggle when none are present.
+@onready var bottom_elements_right: HBoxContainer = %BottomElementsRight
 ## A manual save button.
 @onready var button_save: Button = %ButtonSave
 ## A toggle-able button to enable/disable auto saving.
 @onready var button_auto_save: CheckBox = %ButtonAutoSave
+## A label to display the current position of the caret.
 @onready var label_caret_location: Label = %LabelCaretLocation
 
 
 
 func _ready() -> void:
-	# Load script template text from file.
-	script_template_text = ScriptTemplate.new().get_script().source_code
-
 	_setup_icons()
+	_setup_signals()
 
-	# Signal setup.
-	code_edit.text_changed.connect(_on_code_edit_text_changed)
-	code_edit.caret_changed.connect(_on_code_edit_caret_changed)
-
-	button_online_docs.pressed.connect(_on_button_online_docs_pressed)
-
-	list_methods.item_selected.connect(_on_method_list_item_selected)
-	button_sort_methods.toggled.connect(_on_button_sort_methods_toggled)
-
-	button_toggle_sidebar.pressed.connect(_on_button_toggle_sidebar_pressed)
-	button_save.pressed.connect(save_code)
-
-	button_auto_save.toggled.connect(_on_button_auto_save_toggled)
+	# Sync toggle button with setting.
 	button_auto_save.set_pressed_no_signal(save_on_edit)
 
 ## Set up dynamic icons loaded from the editor theme.
@@ -87,80 +68,49 @@ func _setup_icons() -> void:
 
 	button_save.set_button_icon(get_editor_icon(&"Save"))
 
+## Set up all the signals needed for interactive components.
+func _setup_signals() -> void:
+	button_online_docs.pressed.connect(_on_button_online_docs_pressed)
+
+	list_methods.item_selected.connect(_on_method_list_item_selected)
+	button_sort_methods.toggled.connect(_on_button_sort_methods_toggled)
+
+	button_toggle_sidebar.pressed.connect(_on_button_toggle_sidebar_pressed)
+	button_save.pressed.connect(_on_button_save_pressed)
+
+	button_auto_save.toggled.connect(_on_button_auto_save_toggled)
+
+	script_tab_container.tab_changed.connect(_on_script_tab_container_tab_changed)
+
 
 
 ## Override function from the InspectorPanel class.
 func _on_target_set(new_target : Node) -> void:
-	# Change editable & focus depending on target state.
-	if not new_target:
-		code_edit.editable = false
-		code_edit.focus_mode = Control.FOCUS_NONE
-		return
+	if not new_target: return
+
+	var tab_index : int = script_tab_container.check_for_tab(new_target)
+	if tab_index != -1:
+		script_tab_container.set_current_tab(tab_index)
 	else:
-		code_edit.editable = true
-		code_edit.focus_mode = Control.FOCUS_ALL
-
-	var target_script : Script = new_target.get_script()
-	var target_class : String = new_target.get_class()
-
-	# If target already has a script get it and show it.
-	if target_script and target_script.has_source_code():
-		code_edit.text = target_script.source_code
-
-		# Assign current script name based on global name, alternatively on class name.
-		var script_name : String = target_script.get_global_name()
-		if not script_name.is_empty():
-			current_script_name = script_name.to_snake_case() + ".gd"
-		else:
-			current_script_name = target_class.to_snake_case() + ".script.gd"
-
-	# In case there is no script present get the template and extend from current.
-	else:
-		code_edit.text = (
-				"extends " + target_class +
-				"\n\n" +
-				script_template_text)
-
-		current_script_name = target_class.to_snake_case() + ".script.gd(*)"
-
-	update_method_list()
+		var new_tab : Control = script_tab_container.add_tab(new_target)
+		if new_tab is GDScriptCodeEdit:
+			new_tab.caret_changed.connect(_on_code_edit_caret_changed)
+			new_tab.script_data_updated.connect(update_method_list)
 
 
 
-## Save the code currently in the CodeEdit.
-## Crashes the game in "Editor" builds with the reload function, if code is invalid.
-func save_code() -> void:
-	if not is_instance_valid(target): return
-
-	var new_script := GDScript.new()
-	new_script.source_code = code_edit.text
-
-	# Return on Error other than 0(OK).
-	if new_script.reload(): return
-
-	# Update/Set script on target.
-	target.set_script(new_script)
-	# Ensure process and physics_process are running.
-	target.set_process(true)
-	target.set_physics_process(true)
-
-	# Remove "unsaved changes" marker, if present.
-	if current_script_name.ends_with("(*)"):
-		current_script_name = current_script_name.left(-3)
-
-	# TODO: Might move this and auto-saving on a timer, to reduce repeat calls.
-	update_method_list()
-
+## Update the list of methods in the bottom of the sidebar.
+## TODO: This kinda needs a version for GDScriptDocumentation.
 func update_method_list() -> void:
+	var current_tab : Control = script_tab_container.get_current_tab_control()
 	list_methods.clear()
 
-	var script := GDScript.new()
-	script.source_code = code_edit.text
+	var script_method_list : Array[Dictionary]
+	if current_tab is GDScriptCodeEdit:
+		script_method_list = current_tab.get_script_method_list()
+	else: return
 
-	# Return on Error other than 0(OK).
-	if script.reload(): return
-
-	for method : Dictionary in script.get_script_method_list():
+	for method : Dictionary in script_method_list:
 		list_methods.add_item(method.name)
 
 	if sort_method_list_alphabetically:
@@ -168,19 +118,27 @@ func update_method_list() -> void:
 
 
 
-## Called whenever edits are being made inside the CodeEdit.
-func _on_code_edit_text_changed() -> void:
-	# Append "unsaved changes" marker.
-	if not current_script_name.ends_with("(*)"):
-		current_script_name += "(*)"
+func _on_script_tab_container_tab_changed(index : int) -> void:
+	var current_tab : Control = script_tab_container.get_tab_control(index)
+	var is_type_code_edit : bool = current_tab is GDScriptCodeEdit
+	var is_type_documentation : bool = current_tab is GDScriptDocumentation
 
-	# Save if autosave is turned on.
-	if save_on_edit: save_code()
+	top_menu_bar.set_menu_hidden(1, not is_type_code_edit)
+	top_menu_bar.set_menu_hidden(3, not is_type_code_edit)
+
+	sidebar_methods.set_visible(is_type_code_edit or is_type_documentation)
+
+	bottom_elements_right.set_visible(is_type_code_edit)
+
+	update_method_list()
+
 
 ## Called when the CodeEdit's caret moves.
 func _on_code_edit_caret_changed() -> void:
-	var new_text : String = "%4d:%4d" % [code_edit.get_caret_line(), code_edit.get_caret_column()]
-	label_caret_location.text = new_text
+	var current_tab : Control = script_tab_container.get_current_tab_control()
+	if current_tab is GDScriptCodeEdit:
+		var caret_position : Vector2i = current_tab.get_caret_position()
+		label_caret_location.text = "%4d:%4d" % [caret_position.x, caret_position.y]
 
 ## Opens the online Godot documentation for the current engine version.
 func _on_button_online_docs_pressed() -> void:
@@ -195,15 +153,22 @@ func _on_button_toggle_sidebar_pressed() -> void:
 	else:
 		button_toggle_sidebar.set_button_icon(get_editor_icon(&"Forward"))
 
+func _on_button_save_pressed() -> void:
+	var current_tab : Control = script_tab_container.get_current_tab_control()
+	if current_tab is GDScriptCodeEdit:
+		current_tab.save_code()
+
 ## Toggle auto saving.
 func _on_button_auto_save_toggled(toggled_on : bool) -> void:
 	save_on_edit = toggled_on
 
 ## Move to selected method, called when a method item is clicked in the list.
 func _on_method_list_item_selected(index : int) -> void:
-	var target_function : String = list_methods.get_item_text(index)
-	var result_line : int = code_edit.search(target_function + "(", 0, 0, 0).y
-	code_edit.set_caret_line(result_line)
+	var current_tab : Control = script_tab_container.get_current_tab_control()
+	var target_method : String = list_methods.get_item_text(index)
+
+	if current_tab is GDScriptCodeEdit:
+		current_tab.jump_to_method(target_method)
 
 ## Toggle sorting of the method list.
 func _on_button_sort_methods_toggled(toggled_on : bool) -> void:

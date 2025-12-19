88568c1fabeeef40399afc07d9f41996d0e3b5a3
Holy-Person <holyochi@gmail.com>
2025-12-19 22:01:40 +0100

    Barebone Code Edit Scene
    To be instantiated by the script editor, still very barebone, both in scene and script.

---
+++ barkvr-system/ui/3dPanel/editmode/inspector/components/gdscript_code_edit.gd
@@ -0,0 +1,134 @@
+class_name GDScriptCodeEdit
+extends CodeEdit
+## A custom CodeEdit meant for editing GDScript in the inspector's code panel.
+
+
+
+signal script_name_updated()
+signal script_data_updated()
+
+## The name of the currently selected script.
+var script_name : String
+## A copy of the currently saved script, used for the method list.
+var script_data : GDScript
+
+var has_unsaved_changes : bool = false
+
+## The node this script belongs to, used as a semi-temporary means to connect scripts.
+## Preferrably scripts should be just a script and have some connection system instead.
+var node_target : Node
+
+
+
+func _ready() -> void:
+	text_changed.connect(_on_text_changed)
+
+
+
+func set_script_from_node(target : Node) -> void:
+	if not target: return
+
+	node_target = target
+
+	var target_script : Script = target.get_script()
+	var target_class : String = target.get_class()
+
+	# If target already has a script get it and show it.
+	if target_script and target_script.has_source_code():
+		text = target_script.source_code
+
+		# Assign current script name based on global name, alternatively on class name.
+		var global_script_name : String = target_script.get_global_name()
+		if not global_script_name.is_empty():
+			script_name = global_script_name.to_snake_case()
+			# Global script names can "try" to override already existing ones.
+			# This crashes the game in editor builds.
+			# This causes saves to fail without notifying the user in export builds.
+			editable = false
+		else:
+			script_name = target_class.to_snake_case() + ".script"
+		update_script_data(target_script)
+
+	# In case there is no script present get the template and extend from current.
+	else:
+		# Load script template file and get store its source code.
+		var script_template_text : String = load("uid://cjfm48dxhjdhi").new().get_script().source_code
+		# Dynamically add class to the script file and set as text.
+		text = (
+				"extends " + target_class +
+				"\n\n" +
+				script_template_text)
+
+		script_name = target_class.to_snake_case() + ".script"
+		has_unsaved_changes = true
+		update_script_data()
+
+## Update the current script data, reloads text into a script if none is provided.
+func update_script_data(script : GDScript = null) -> GDScript:
+	if not script and editable:
+		script = GDScript.new()
+		script.source_code = text
+
+		# Return on Error other than 0(OK).
+		if script.reload(): return
+
+	script_data = script
+	script_data_updated.emit()
+	return script
+
+func get_script_name_unsaved() -> String:
+	if has_unsaved_changes:
+		return script_name + ".gd(*)"
+	else:
+		return script_name + ".gd"
+
+func get_script_method_list() -> Array[Dictionary]:
+	if not script_data: return []
+	var method_list : Array[Dictionary] = script_data.get_script_method_list()
+	var filtered_method_list = method_list.filter(
+			func(method): return search("func " + method.name + "(", 0, 0, 0).y != -1)
+	return filtered_method_list
+
+func set_script_name(new_name : String) -> void:
+	script_name = new_name
+
+
+
+## Save the code currently in text.
+## Crashes the game in "Editor" builds with the reload function, if code is invalid.
+func save_code() -> void:
+	if not is_instance_valid(node_target): return
+	if not editable: return
+
+	update_script_data()
+
+	# Update/Set script on target.
+	node_target.set_script(script_data)
+	# Ensure process and physics_process are running.
+	node_target.set_process(true)
+	node_target.set_physics_process(true)
+
+	has_unsaved_changes = false
+
+func jump_to_method(method : String) -> void:
+	var method_line : int = search("func " + method + "(", 0, 0, 0).y
+	# Despite what the 4.5 documentation might suggest, "adjust_viewport" does not actually center.
+	# As such, it has been disabled and center_viewport_to_caret is being used instead.
+	set_caret_line(method_line, false)
+	center_viewport_to_caret()
+
+
+
+## Called when the text has been changed in any way.
+func _on_text_changed() -> void:
+	pass
+
+## A timeout from text_changed to prevent saving on every tiny change.
+func _on_text_changed_timeout() -> void:
+	pass
+
+func get_caret_position() -> Vector2i:
+	var caret_position := Vector2i(
+			get_caret_line(),
+			get_caret_column())
+	return caret_position

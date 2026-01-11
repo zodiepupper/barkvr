e0135b798e99a18baecc285656931442d1fa0093
Holy-Person <holyochi@gmail.com>
2026-01-11 18:26:04 +0100

    Rename Panels
    Decided on this to align better with Godot's naming.
    "Inspector" in the regular Godot UI became "Properties" due to the fact that the whole thing itself is already called the "Inspector". It could be renamed into "Editor" or something of the sort, but that might be a pain in regards to the actual "Editor" and autocomplete.

--- barkvr-system/ui/3dPanel/editmode/inspector/inspector_main.gd
+++ barkvr-system/ui/3dPanel/editmode/inspector/inspector_main.gd
@@ -6,7 +6,7 @@ extends Control
 @onready var tab_right: TabContainer = %TabRight
 
 ## The left side of the inspector, a scene inspector.
-@onready var scene_inspector: Control = %SceneInspector
+@onready var scene_inspector: Control = %Scene
 
 
 
@@ -21,10 +21,10 @@ func _on_node_selection_changed(new_selection : Node) -> void:
 	LocalGlobals.is_inspector_loading = false
 
 	# Get all currently attached tabs.
-	var target_tabs : Array[Node] = tab_left.get_children() + tab_right.get_children()
+	var target_tabs: Array[Node] = tab_left.get_children() + tab_right.get_children()
 
 	# Set target on all possible tabs.
-	for tab : Node in target_tabs:
+	for tab: Node in target_tabs:
 		if tab.has_method(&"set_target"):
 			tab.set_target(new_selection)
 

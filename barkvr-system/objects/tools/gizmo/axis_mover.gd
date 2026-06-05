c188f053369569c590f67a7941ec8aca48566697
zodiepupper <zodie@pupper.dev>
2026-06-04 23:16:06 -0400

    should have committed this after the next commit but oh well, this makes it so the gizmo doesn't need to expand it's own colliders since we now do a planar projection when the user starts holding down a press

--- barkvr-system/objects/tools/gizmo/axis_mover.gd
+++ barkvr-system/objects/tools/gizmo/axis_mover.gd
@@ -24,14 +24,14 @@ func laser_input(data: Dictionary) -> void:
 
 	if gizmo.target and prev_click_pos != data.position and data.index == interaction_index:
 		if prev_click_pos and data.pressed:
-			_set_colliders(true)
+			#_set_colliders(true)
 			var tmppos: Vector3 = gizmo.target.global_position
 			tmppos[_axis] = data.position[_axis] - _offset[_axis]
 			gizmo.target.global_position = tmppos
 			prev_click_pos = data.position
 		else:
 			interaction_index = -1
-			_set_colliders(false)
+			#_set_colliders(false)
 			prev_click_pos = data.position
 			_offset = data.position-gizmo.target.global_position
 			if is_instance_valid(Engine.get_singleton(&"event_manager")):
@@ -44,42 +44,42 @@ func laser_input(data: Dictionary) -> void:
 	elif data.has("hovering"):
 		if !data.hovering:
 			interaction_index = -1
-			_set_colliders(false)
+			#_set_colliders(false)
 			prev_click_pos = Vector3()
 			_offset = Vector3()
 
-func _set_colliders(is_drag: bool = false) -> void:
-	match _axis:
-		"x":
-			if is_drag:
-				zcol.disabled = true
-				ycol.disabled = true
-				xcol.shape.size.x = 10000000000.0
-				xcol.shape.size.y = 10000000000.0
-			else:
-				zcol.disabled = false
-				ycol.disabled = false
-				xcol.shape.size.x = 1.1
-				xcol.shape.size.y = 0.1
-		"y":
-			if is_drag:
-				zcol.disabled = true
-				xcol.disabled = true
-				ycol.shape.size.x = 10000000000.0
-				ycol.shape.size.y = 10000000000.0
-			else:
-				zcol.disabled = false
-				xcol.disabled = false
-				ycol.shape.size.x = 1.1
-				ycol.shape.size.y = 0.1
-		"z":
-			if is_drag:
-				xcol.disabled = true
-				ycol.disabled = true
-				zcol.shape.size.x = 10000000000.0
-				zcol.shape.size.y = 10000000000.0
-			else:
-				xcol.disabled = false
-				ycol.disabled = false
-				zcol.shape.size.x = 1.1
-				zcol.shape.size.y = 0.1
+#func _set_colliders(is_drag: bool = false) -> void:
+	#match _axis:
+		#"x":
+			#if is_drag:
+				#zcol.disabled = true
+				#ycol.disabled = true
+				#xcol.shape.size.x = 10000000000.0
+				#xcol.shape.size.y = 10000000000.0
+			#else:
+				#zcol.disabled = false
+				#ycol.disabled = false
+				#xcol.shape.size.x = 1.1
+				#xcol.shape.size.y = 0.1
+		#"y":
+			#if is_drag:
+				#zcol.disabled = true
+				#xcol.disabled = true
+				#ycol.shape.size.x = 10000000000.0
+				#ycol.shape.size.y = 10000000000.0
+			#else:
+				#zcol.disabled = false
+				#xcol.disabled = false
+				#ycol.shape.size.x = 1.1
+				#ycol.shape.size.y = 0.1
+		#"z":
+			#if is_drag:
+				#xcol.disabled = true
+				#ycol.disabled = true
+				#zcol.shape.size.x = 10000000000.0
+				#zcol.shape.size.y = 10000000000.0
+			#else:
+				#xcol.disabled = false
+				#ycol.disabled = false
+				#zcol.shape.size.x = 1.1
+				#zcol.shape.size.y = 0.1
